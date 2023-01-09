29eccfe2fff4905a8b01a064ee776943fd496fea
gauenk <kent.gauen@gmail.com>
2023-01-09 13:41:51 -0500

    added warning

--- lib/dev_basics/exps.py
+++ lib/dev_basics/exps.py
@@ -8,15 +8,18 @@ import yaml
 from .mesh import mesh_groups,add_cfg
 
 def load(fn): # read + unpack
+    print("[dev_basics.exps] Moving this to cache_io.")
     edata = read(fn)
     return unpack(edata)
 
 def read(fn):
+    print("[dev_basics.exps] Moving this to cache_io.")
     with open(fn,"r") as stream:
         data = yaml.safe_load(stream)
     return data
 
 def unpack(edata):
+    print("[dev_basics.exps] Moving this to cache_io.")
     cfg = edata['cfg']
     groups = [v for g,v in edata.items() if "group" in g]
     grids = [v for g,v in edata.items() if "global_grids" in g]
