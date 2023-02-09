4e36a072a35f0b77d25bb4d5b6282c3cce2726b8
gauenk <kent.gauen@gmail.com>
2023-02-08 21:31:43 -0500

    added model bench

--- lib/dev_basics/trte/__init__.py
+++ lib/dev_basics/trte/__init__.py
@@ -6,3 +6,4 @@ Shared python scripts for training and testing
 
 from . import train
 from . import test
+from . import bench
---
+++ lib/dev_basics/trte/bench.py
@@ -0,0 +1,82 @@
+"""
+
+Benchmark Forward/Backward Runtime/Memory
+
+"""
+
+# -- imports --
+import numpy as np
+import torch as th
+
+# -- model io --
+import importlib
+
+# -- data --
+import data_hub
+
+# -- optical flow --
+from dev_basics import flow
+
+# -- bench --
+from dev_basics.utils.timer import ExpTimer,TimeIt
+from dev_basics.utils.gpu_mem import GpuMemer,MemIt
+
+
+def run(cfg):
+
+    # -- timer/memer --
+    timer = ExpTimer()
+    memer = GpuMemer()
+
+    # -- load --
+    vid = load_sample(cfg)
+    model = load_model(cfg)
+    flows = flow.orun(vid,cfg.flow)
+
+    # -- init --
+    model(vid,flows)
+
+    # -- bench fwd --
+    with th.no_grad():
+        with TimeIt(timer,"fwd_nograd"):
+            with MemIt(memer,"fwd_nograd"):
+                model(vid,flows)
+
+    # -- bench fwd --
+    with TimeIt(timer,"fwd"):
+        with MemIt(memer,"fwd"):
+            model(vid,flows)
+
+    # -- compute grad --
+    deno = model(vid,flows)
+    error = th.randn_like(deno)
+    loss = th.mean((error - deno)**2)
+
+    # -- bench fwd --
+    with TimeIt(timer,"bwd"):
+        with MemIt(memer,"bwd"):
+            loss.backward()
+
+    # -- fill results --
+    results = {}
+    for key,val in timer.items():
+        results[key] = val
+    for key,(res,alloc) in memer.items():
+        results["res_%s"%key] = res
+        results["alloc_%s"%key] = alloc
+
+    print(results)
+    return results
+
+def load_model(cfg):
+    return importlib.import_module(cfg.python_module).load_model(cfg)
+
+def load_sample(cfg):
+    # -- init data --
+    imax = 255.
+    data,loaders = data_hub.sets.load(cfg)
+    indices = data_hub.filter_nframes(data[cfg.dset],cfg.vid_name,
+                                      cfg.frame_start,cfg.nframes)
+    sample = data[cfg.dset][indices[0]]
+    return sample['noisy'][None,:].to(cfg.device)
+
